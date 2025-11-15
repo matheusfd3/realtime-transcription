665c4aad9b5d0fbf4c95d89770f49a84c4ea1fa8
Matheus Ferreira <matheusfdantas03@gmail.com>
2025-11-14 23:01:47 -0300

    Implement package installation check and prompt user for missing dependencies

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 if __name__ == '__main__':
-    pass
+    from install_packages import check_and_install_packages
+    check_and_install_packages([
+        {
+            'import_name': 'rich',
+        }      
+    ])
